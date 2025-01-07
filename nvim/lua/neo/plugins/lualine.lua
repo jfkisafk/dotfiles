567b263a7d20babfecaa647b8ddefd34cb1df18f
stelo <contact@stelo.dev>
2025-01-07 02:20:02 -0800

    feat(nvim): adds yazi to nvim

--- nvim/lua/neo/plugins/lualine.lua
+++ nvim/lua/neo/plugins/lualine.lua
@@ -1,26 +1,58 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	event = "ColorScheme",
-	config = function()
-		local palette = require("rose-pine.palette")
-		local lazy_status = require("lazy.status")
+  "nvim-lualine/lualine.nvim",
+  event = "ColorScheme",
+  config = function()
+    local p = require("rose-pine.palette")
+    local lazy_status = require("lazy.status")
+    local theme = {
+      normal = {
+        a = { bg = p.rose, fg = p.base, gui = "bold" },
+        b = { fg = p.rose },
+        c = { fg = p.text },
+      },
+      insert = {
+        a = { bg = p.foam, fg = p.base, gui = "bold" },
+        b = { fg = p.foam },
+        c = { fg = p.text },
+      },
+      visual = {
+        a = { bg = p.iris, fg = p.base, gui = "bold" },
+        b = { fg = p.iris },
+        c = { fg = p.text },
+      },
+      replace = {
+        a = { bg = p.pine, fg = p.base, gui = "bold" },
+        b = { fg = p.pine },
+        c = { fg = p.text },
+      },
+      command = {
+        a = { bg = p.love, fg = p.base, gui = "bold" },
+        b = { fg = p.love },
+        c = { fg = p.text },
+      },
+      inactive = {
+        a = { bg = p.base, fg = p.muted, gui = "bold" },
+        b = { fg = p.muted },
+        c = { fg = p.muted },
+      },
+    }
 
-		require("lualine").setup({
-			options = {
-				theme = "rose-pine",
-			},
-			sections = {
-				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = palette.love },
-					},
-					{ "encoding" },
-					{ "fileformat" },
-					{ "filetype" },
-				},
-			},
-		})
-	end,
+    require("lualine").setup({
+      options = {
+        theme = theme,
+      },
+      sections = {
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = p.love },
+          },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
+        },
+      },
+    })
+  end,
 }
