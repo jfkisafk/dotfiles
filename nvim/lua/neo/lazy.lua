cc70ee035f2418f21ca520b78d9a3b8631e89a6a
stelo <contact@stelo.dev>
2025-01-07 06:23:05 -0800

    refactor(nvim): changed the file structure

--- nvim/lua/neo/lazy.lua
+++ nvim/lua/neo/lazy.lua
@@ -1,22 +1,29 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ import = "neo.plugins" }, {
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		notify = false,
-	},
+require("lazy").setup({
+  { import = "neo.plugins" },
+  { import = "neo.plugins.code" },
+  { import = "neo.plugins.ui" },
+  { import = "neo.plugins.completions" },
+  { import = "neo.plugins.explorer" },
+  { import = "neo.plugins.git" }
+}, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
 })
