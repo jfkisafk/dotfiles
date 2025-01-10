624e2e4d5ea89f90ac71211902b03fa9ee682afa
stelo <contact@stelo.dev>
2025-01-10 02:18:24 -0800

    perf(nvim): made visual optimizations

--- nvim/lua/neo/plugins/init.lua
+++ nvim/lua/neo/plugins/init.lua
@@ -1,4 +1,4 @@
 return {
-	"ThePrimeagen/vim-be-good",
-	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+  "ThePrimeagen/vim-be-good", -- learn how to use vim
+  "nvim-lua/plenary.nvim",   -- lua functions that many plugins use
 }
