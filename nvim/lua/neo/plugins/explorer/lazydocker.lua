d08506adacb2e272884d4942c6c0ab3ca254b5ec
stelo <contact@stelo.dev>
2025-01-27 18:24:19 -0800

    feat(nvim): add lazydocker config

---
+++ nvim/lua/neo/plugins/explorer/lazydocker.lua
@@ -0,0 +1,11 @@
+return {
+  "crnvl96/lazydocker.nvim",
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    { "<leader>ld", "<cmd>LazyDocker<cr>", desc = "Open lazy docker" },
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+}
