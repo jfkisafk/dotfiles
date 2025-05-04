75d5cbf9f2cef36f2e491d9dc3dacd6e3e43bd2c
stelo <contact@stelo.dev>
2025-05-03 22:31:46 -0700

    add keymaps for tab management

--- nvim/lua/neo/plugins/code/debug.lua
+++ nvim/lua/neo/plugins/code/debug.lua
@@ -50,11 +50,9 @@ return {
 			dap.configurations.cs = {
 				{
 					type = "coreclr",
-					name = "launch - netcoredbg",
-					request = "launch",
-					program = function()
-						return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
-					end,
+					name = "attach - netcoredbg",
+					request = "attach",
+					processId = require("dap.utils").pick_process,
 				},
 			}
 
--- nvim/lua/neo/plugins/code/test.lua
+++ nvim/lua/neo/plugins/code/test.lua
@@ -1,29 +1,29 @@
 return {
-  "nvim-neotest/neotest",
-  dependencies = {
-    "Issafalcon/neotest-dotnet",
-    "nvim-neotest/nvim-nio",
-    "nvim-neotest/neotest-jest",
-  },
-  config = function()
-    require("neotest").setup({
-      adapters = {
-        require("neotest-dotnet"),
-        require("neotest-jest"),
-      },
-      output = {
-        enabled = false,
-        open_on_run = false,
-      },
-    })
-  end,
-  keys = {
-    { "<leader>tt", "<cmd>lua require'neotest'.run.run()<cr>",                     desc = "Test Nearest" },
-    { "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", desc = "Test File" },
-    { "<leader>td", "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", desc = "Debug Test" },
-    { "<leader>ts", "<cmd>lua require('neotest').run.stop()<cr>",                  desc = "Test Stop" },
-    { "<leader>ta", "<cmd>lua require('neotest').run.attach()<cr>",                desc = "Attach Test" },
-    { "<leader>to", "<cmd>Neotest output<cr>",                                     desc = "Test Output" },
-    { "<leader>ty", "<cmd>Neotest summary<cr>",                                    desc = "Test Summary" },
-  },
+	"nvim-neotest/neotest",
+	dependencies = {
+		"Issafalcon/neotest-dotnet",
+		"nvim-neotest/nvim-nio",
+		"nvim-neotest/neotest-jest",
+	},
+	config = function()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-dotnet"),
+				require("neotest-jest"),
+			},
+			output = {
+				enabled = false,
+				open_on_run = false,
+			},
+		})
+	end,
+	keys = {
+		{ "<leader>tt", "<cmd>lua require'neotest'.run.run()<cr>", desc = "Test Nearest" },
+		{ "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", desc = "Test File" },
+		{ "<leader>td", "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", desc = "Debug Test" },
+		{ "<leader>ts", "<cmd>lua require('neotest').run.stop()<cr>", desc = "Test Stop" },
+		{ "<leader>ta", "<cmd>lua require('neotest').run.attach()<cr>", desc = "Attach Test" },
+		{ "<leader>to", "<cmd>Neotest output<cr>", desc = "Test Output" },
+		{ "<leader>ty", "<cmd>Neotest summary<cr>", desc = "Test Summary" },
+	},
 }
