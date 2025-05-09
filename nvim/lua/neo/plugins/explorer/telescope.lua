8ae5267cb37abda02b0c544c368a083b3070f422
stelo <contact@stelo.dev>
2025-05-09 02:04:24 -0700

    add octo plugin

--- nvim/lua/neo/plugins/explorer/telescope.lua
+++ nvim/lua/neo/plugins/explorer/telescope.lua
@@ -6,6 +6,7 @@ return {
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-tree/nvim-web-devicons",
 		"folke/todo-comments.nvim",
+		"nvim-telescope/telescope-frecency.nvim",
 		"andrew-george/telescope-themes",
 	},
 	config = function()
@@ -15,6 +16,7 @@ return {
 		telescope.setup({
 			defaults = {
 				path_display = { "smart" },
+				layout_config = { horizontal = { preview_width = 0.7 } },
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
@@ -27,6 +29,7 @@ return {
 
 		telescope.load_extension("fzf")
 		telescope.load_extension("themes")
+		telescope.load_extension("frecency")
 
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
@@ -34,7 +37,7 @@ return {
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>fc", "<cmd>Telescope treesitter<cr>", { desc = "Find treesitter" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 		keymap.set("n", "<leader>th", "<cmd>Telescope themes<cr>", { desc = "Explore themes in Switcher" })
 	end,
