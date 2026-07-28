f1a84effd7cf0af2400b58289787614bec572486
stelo <stelo.prakash@docusign.com>
2026-07-27 19:00:03 -0700

    feat: add herdr vim nav

--- nvim/lua/neo/plugins/explorer/nvim-tmux-nav.lua
+++ nvim/lua/neo/plugins/explorer/nvim-tmux-nav.lua
@@ -1,20 +1,9 @@
 return {
   {
-    "christoomey/vim-tmux-navigator",
+    "paulbkim-dev/vim-herdr-navigation",
     event = "VeryLazy",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
+    config = function()
+      dofile(vim.fn.stdpath("data") .. "/lazy/vim-herdr-navigation/editor/nvim.lua")
+    end,
   },
 }
